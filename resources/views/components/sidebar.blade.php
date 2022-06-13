401f2b17e6c3895bdeacaf5b75cd00da59461ce3
Galih <hgalih@protonmail.com>
2022-06-13 14:39:59 +0700

    fix activePage

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -22,10 +22,10 @@
 
     <title>Faumation</title>
 </head>
-@php
-        $activePage = basename($_SERVER['PHP_SELF'], '.php');
-@endphp
 <body class="bg-gray-800">
+  @php
+  $activePage = request()->segment(count(request()->segments()))
+  @endphp
     <!-- This example requires Tailwind CSS v2.0+ -->
     <!--
   This example requires updating your template:
@@ -163,7 +163,7 @@
                     <nav class="mt-5 flex-1 px-2 space-y-1">
                         <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
                         <a href="{{ route('kontrol') }}"
-                            class="  text-white group flex items-center px-2 py-2 text-sm font-medium rounded-md <?= $activePage == 'index' ? 'bg-indigo-800' : '' ?>">
+                            class="  text-white group flex items-center px-2 py-2 text-sm font-medium rounded-md <?= $activePage == '' ? 'bg-indigo-800' : '' ?>">
                             <!--
               Heroicon name: outline/home
 
